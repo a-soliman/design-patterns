6b8893d4a96a5583bbbc122a640ffe02322536bd
Ahmed Soliman <ahmed.soliman@programmer.net>
2020-05-19 15:17:04 -0700

    structural pproach to FACTORY PATTERN

--- 02-factories/main.go
+++ 02-factories/main.go
@@ -15,6 +15,17 @@ func NewEmployeeFactory(postion string, annualIncome int) func(name string) *Emp
 	}
 }
 
+// EmployeeFactory << STRUCTURAL APPROACH >>
+type EmployeeFactory struct {
+	Position     string
+	AnnualIncome int
+}
+
+// Create returns a pointer to a newly created employee
+func (e *EmployeeFactory) Create(name string) *Employee {
+	return &Employee{name, e.Position, e.AnnualIncome}
+}
+
 func main() {
 	// << FUNCTIONAL APPROACH >>
 	developerFactory := NewEmployeeFactory("Developer", 150000)
@@ -25,4 +36,10 @@ func main() {
 
 	fmt.Println(john)
 	fmt.Println(jane)
+
+	// << STRUCTURAL APPROACH >>
+	ceoFactory := EmployeeFactory{"CEO", 200000}
+
+	sam := ceoFactory.Create("Sam")
+	fmt.Println(sam)
 }
